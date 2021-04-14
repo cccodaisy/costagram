235b1e456fb2e8e7ab17f6caaec20588b944b86d
Dasun Lee <dasun.lee@saracen.co.kr>
2021-04-14 16:40:16 +0900

    first commit

---
+++ lib/feed_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'instagram',
+          style: TextStyle(
+            fontFamily: 'VeganStyle',
+            color: Colors.black87
+          )
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: feedListBuilder,
+        itemCount: 30,
+      ),
+    );
+  }
+
+  Widget feedListBuilder(BuildContext context, int index){
+    return Container(
+      color: Colors.accents[index % Colors.accents.length],
+      height: 100,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/home_page.dart';
+import 'package:flutter_app/constants/material_white.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: white)
+    );
+  }
+}
