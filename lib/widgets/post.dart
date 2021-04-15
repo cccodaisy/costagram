6addefe53bafca8167e3493ee8c1114ef6b279d7
Dasun Lee <dasun.lee@saracen.co.kr>
2021-04-15 18:04:37 +0900

    add progress.dart

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,17 +1,67 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:costagram/widgets/my_progress_indicator.dart';
 import 'package:flutter/material.dart';
 
 class Post extends StatelessWidget {
   final int index;
+  Size size;
 
-  const Post(this.index, {
+  Post(
+    this.index, {
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.accents[index % Colors.accents.length],
-      height: 100,
+    if (size == null) size = MediaQuery.of(context).size;
+
+    return Column(
+      children: [
+        _postHeader(),
+        _postImage(),
+      ],
     );
   }
+
+  Widget _postHeader(){
+    return Row(
+      children: <Widget>[
+        CachedNetworkImage(
+          imageUrl: 'https://picsum.photos/100',
+          width: 30,
+          height: 30,
+        ),
+        Text('username'),
+        IconButton(
+          onPressed: null,
+            icon: Icon(
+              Icons.more_horiz,
+              color: Colors.black87,
+            ),
+        )
+      ],
+    );
+  }
+
+  CachedNetworkImage _postImage() {
+    return CachedNetworkImage(
+        imageUrl: 'https://picsum.photos/id/$index/200/200',
+        placeholder: (BuildContext context, String url) {
+          return MyProgressIndicator(
+              containerSize: size.width
+          );
+        },
+        imageBuilder: (BuildContext context, ImageProvider imageProvider) {
+          return AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                  image:
+                      DecorationImage(image: imageProvider, fit: BoxFit.cover)),
+            ),
+          );
+        },
+      );
+  }
 }
+
