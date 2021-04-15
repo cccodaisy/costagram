ffcca96e3f5d67acbc954586a60cdc3c3c56d90a
Dasun Lee <dasun.lee@saracen.co.kr>
2021-04-15 09:20:44 +0900

    add post.dart

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,16 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:costagram/widgets/post.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
+      appBar: CupertinoNavigationBar(
+        leading: IconButton(
+          onPressed: null,
+          icon: Icon(
+            Icons.camera_alt,
+            color: Colors.black87
+          ),
+        ),
+        middle: Text(
           'instagram',
           style: TextStyle(
             fontFamily: 'VeganStyle',
             color: Colors.black87
           )
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              icon: ImageIcon(
+                AssetImage('assets/images/actionbar_camera.png'),
+                color: Colors.black87,
+              ),
+              onPressed: null,
+            ),
+            IconButton(
+              icon: ImageIcon(
+                AssetImage('assets/images/actionbar_camera.png'),
+                color: Colors.black87,
+              ),
+              onPressed: null,
+            )
+          ],
         ),
       ),
       body: ListView.builder(
@@ -21,9 +49,6 @@ class FeedScreen extends StatelessWidget {
   }
 
   Widget feedListBuilder(BuildContext context, int index){
-    return Container(
-      color: Colors.accents[index % Colors.accents.length],
-      height: 100,
-    );
+    return Post(index);
   }
 }
