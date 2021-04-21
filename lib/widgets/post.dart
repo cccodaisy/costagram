bed9235159cea37576d77dc1d495df5d7256396e
Dasun Lee <dasun.lee@saracen.co.kr>
2021-04-21 17:58:04 +0900

    #1.0 Feed Screen Layout

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,43 +1,102 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:costagram/constants/common_size.dart';
+import 'package:costagram/widgets/comment.dart';
 import 'package:costagram/widgets/my_progress_indicator.dart';
+import 'package:costagram/widgets/rounded_avatar.dart';
 import 'package:flutter/material.dart';
 
 class Post extends StatelessWidget {
   final int index;
   Size size;
 
-  Post(
-    this.index, {
+  Post(this.index, {
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (size == null) size = MediaQuery.of(context).size;
+    if (size == null) size = MediaQuery
+        .of(context)
+        .size;
 
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         _postHeader(),
         _postImage(),
+        _postActions(),
+        _postLikes(),
+        _postCaption(),
       ],
     );
   }
 
-  Widget _postHeader(){
+  Widget _postCaption() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: common_gap,
+        vertical: common_xxs_gap
+      ),
+      child: Comment(
+        showImage: false,
+        username: "testingUser",
+        text: "I have money",
+      ),
+    );
+  }
+
+  Padding _postLikes() {
+    return Padding(
+      padding: const EdgeInsets.only(left: common_gap),
+      child: Text(
+        '12000 likes',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Row _postActions() {
     return Row(
       children: <Widget>[
-        CachedNetworkImage(
-          imageUrl: 'https://picsum.photos/100',
-          width: 30,
-          height: 30,
+        IconButton(
+          icon: ImageIcon(AssetImage('assets/images/bookmark.png')),
+          onPressed: null,
+          color: Colors.black87,
         ),
-        Text('username'),
+        IconButton(
+          icon: ImageIcon(AssetImage('assets/images/comment.png')),
+          onPressed: null,
+          color: Colors.black87,
+        ),
+        IconButton(
+          icon: ImageIcon(AssetImage('assets/images/direct_message.png')),
+          onPressed: null,
+          color: Colors.black87,
+        ),
+        Spacer(),
+        IconButton(
+          icon: ImageIcon(AssetImage('assets/images/heart_selected.png')),
+          onPressed: null,
+          color: Colors.black87,
+        ),
+      ],
+    );
+  }
+
+  Widget _postHeader() {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(common_xxs_gap),
+          child: RoundedAvatar(),
+        ),
+        Expanded(child: Text('username')),
         IconButton(
           onPressed: null,
-            icon: Icon(
-              Icons.more_horiz,
-              color: Colors.black87,
-            ),
+          icon: Icon(
+            Icons.more_horiz,
+            color: Colors.black87,
+          ),
         )
       ],
     );
@@ -45,23 +104,24 @@ class Post extends StatelessWidget {
 
   CachedNetworkImage _postImage() {
     return CachedNetworkImage(
-        imageUrl: 'https://picsum.photos/id/$index/200/200',
-        placeholder: (BuildContext context, String url) {
-          return MyProgressIndicator(
-              containerSize: size.width
-          );
-        },
-        imageBuilder: (BuildContext context, ImageProvider imageProvider) {
-          return AspectRatio(
-            aspectRatio: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                  image:
-                      DecorationImage(image: imageProvider, fit: BoxFit.cover)),
-            ),
-          );
-        },
-      );
+      imageUrl: "https://picsum.photos/id/$index/200/200",
+      placeholder: (BuildContext context, String url) {
+        return MyProgressIndicator(
+            containerSize: size.width
+        );
+      },
+      imageBuilder: (BuildContext context, ImageProvider imageProvider) {
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Container(
+            decoration: BoxDecoration(
+                image:
+                DecorationImage(image: imageProvider, fit: BoxFit.cover)),
+          ),
+        );
+      },
+    );
   }
 }
+
 
